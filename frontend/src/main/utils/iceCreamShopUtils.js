3b12ae95d39c89826fa07973231b6b0d61904294
khtran1 <khtran@ucsb.edu>
2023-04-25 14:28:24 -0700

    kt - implemented utils

--- frontend/src/main/utils/iceCreamShopUtils.js
+++ frontend/src/main/utils/iceCreamShopUtils.js
@@ -12,3 +12,78 @@ const get = () => {
     }
     return iceCreamShopCollection;
 };
+
+const getById = (id) => {
+    if (id === undefined) {
+        return { "error": "id is a required parameter" };
+    }
+    const iceCreamShopCollection = get();
+    const iceCreamShops = iceCreamShopCollection.iceCreamShops;
+
+    /* eslint-disable-next-line eqeqeq */ // we really do want == here, not ===
+    const index = iceCreamShops.findIndex((r) => r.id == id);
+    if (index === -1) {
+        return { "error": `iceCreamShop with id ${id} not found` };
+    }
+    return { iceCreamShop: iceCreamShops[index] };
+}
+
+// set iceCreamShops in local storage
+const set = (iceCreamShopCollection) => {
+    localStorage.setItem("iceCreamShops", JSON.stringify(iceCreamShopCollection));
+    return iceCreamShopCollection;
+};
+
+// add an iceCreamShop to local storage
+const add = (iceCreamShop) => {
+    const iceCreamShopCollection = get();
+    iceCreamShop = { ...iceCreamShop, id: iceCreamShopCollection.nextId };
+    iceCreamShopCollection.nextId++;
+    iceCreamShopCollection.iceCreamShops.push(iceCreamShop);
+    set(iceCreamShopCollection);
+    return iceCreamShop;
+};
+
+// update an iceCreamShop in local storage
+const update = (iceCreamShop) => {
+    const iceCreamShopCollection = get();
+
+    const iceCreamShops = iceCreamShopCollection.iceCreamShops;
+
+    /* eslint-disable-next-line eqeqeq */ // we really do want == here, not ===
+    const index = iceCreamShops.findIndex((r) => r.id == iceCreamShop.id);
+    if (index === -1) {
+        return { "error": `iceCreamShop with id ${iceCreamShop.id} not found` };
+    }
+    iceCreamShops[index] = iceCreamShop;
+    set(iceCreamShopCollection);
+    return { iceCreamShopCollection: iceCreamShopCollection };
+};
+
+// delete an iceCreamShop from local storage
+const del = (id) => {
+    if (id === undefined) {
+        return { "error": "id is a required parameter" };
+    }
+    const iceCreamShopCollection = get();
+    const iceCreamShops = iceCreamShopCollection.iceCreamShops;
+
+    /* eslint-disable-next-line eqeqeq */ // we really do want == here, not ===
+    const index = iceCreamShops.findIndex((r) => r.id == id);
+    if (index === -1) {
+        return { "error": `iceCreamShop with id ${id} not found` };
+    }
+    iceCreamShops.splice(index, 1);
+    set(iceCreamShopCollection);
+    return { iceCreamShopCollection: iceCreamShopCollection };
+};
+
+const iceCreamShopUtils = {
+    get,
+    getById,
+    add,
+    update,
+    del
+};
+
+export { iceCreamShopUtils };
