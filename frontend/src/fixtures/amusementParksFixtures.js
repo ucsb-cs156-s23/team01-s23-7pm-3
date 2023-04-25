740b79de1ed330902c605c99d923b6f2c1b0717a
Nick Zahlis <nickzahlis@169-231-158-90.wireless.ucsb.edu>
2023-04-25 14:20:29 -0700

    nz - added amusementParksFixtures.js

---
+++ frontend/src/fixtures/amusementParksFixtures.js
@@ -0,0 +1,38 @@
+const amusementParksFixtures = {
+    oneAmusementPark:
+    [
+      {
+       "id": 1,
+        "name": "Disneyland Park",
+        "address": "1313 Disneyland Dr",
+        "description": "Disney-themed amusement park rides"      
+      }
+    ],
+
+    threeAmusementParks:
+    [
+        {
+            "id": 2,
+             "name": "Six Flags Magic Mountain",
+             "address": "26101 Magic Mountain Pkwy",
+             "description": "This park is known for its large roller coasters",      
+        },
+
+        {
+            "id": 3,
+             "name": "LEGOLAND California",
+             "address": "One Legoland Dr",
+             "description": "LEGO themed rides and attractions"  
+        },
+
+        {
+            "id": 4,
+             "name": "Universal Studios Hollywood",
+             "address": "100 Universal City Plaza",
+             "description": "Film studio and theme park with rides and attractions based on movies and television"      
+        },
+        
+    ]
+};
+
+export { amusementParksFixtures };
