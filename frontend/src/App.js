75cb7afc74b2eb8f644faca00e86054982614422
Hardy Xu <zihaoxu@ucsb.edu>
2023-04-27 23:11:47 -0700

    hx - add related links and route for book

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,10 @@ import RestaurantEditPage from "main/pages/Restaurants/RestaurantEditPage";
 import RestaurantIndexPage from "main/pages/Restaurants/RestaurantIndexPage";
 import RestaurantDetailsPage from "main/pages/Restaurants/RestaurantDetailsPage";
 
+import BookCreatePage from "main/pages/Books/BookCreatePage";
+import BookEditPage from "main/pages/Books/BookEditPage";
+import BookIndexPage from "main/pages/Books/BookIndexPage";
+import BookDetailsPage from "main/pages/Books/BookDetailsPage";
 
 function App() {
 
@@ -18,18 +22,22 @@ function App() {
   return (
     <BrowserRouter basename="/team01-s23-7pm-3">
       <Routes>
-        <Route path="/storybook-static" onEnter={reload}/>
+        <Route path="/storybook-static" onEnter={reload} />
         <Route exact path="/" element={<HomePage />} />
 
         <Route exact path="/towns/AvilaBeach" element={<AvilaBeachPage />} />
         <Route exact path="/towns/LosAlamos" element={<LosAlamosPage />} />
         <Route exact path="/towns/ArroyoGrande" element={<ArroyoGrandePage />} />
-        
+
         <Route exact path="/restaurants/create" element={<RestaurantCreatePage />} />
         <Route exact path="/restaurants/edit/:id" element={<RestaurantEditPage />} />
         <Route exact path="/restaurants/details/:id" element={<RestaurantDetailsPage />} />
         <Route exact path="/restaurants/" element={<RestaurantIndexPage />} />
 
+        <Route exact path="/books/create" element={<BookCreatePage />} />
+        <Route exact path="/books/edit/:id" element={<BookEditPage />} />
+        <Route exact path="/books" element={<BookIndexPage />} />
+        <Route exact path="/books/details/:id" element={<BookDetailsPage />} />
       </Routes>
     </BrowserRouter>
   );
